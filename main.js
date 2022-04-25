ac70b34e5121ef8f86055cfbe1db9ab29e56a57d
RomuloLessa <romulo-lessa@hotmail.com>
2022-04-25 15:03:49 -0300

    Final da aula 2

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 /*Abre e fecha menu ao clicar no icone*/
 const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll("nav .toggle")
+const toggle = document.querySelectorAll('nav .toggle')
 
-for(const element of toggle){
-    element.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-    })
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
 }
 
 /*quando clica em item do menu, esconde o menu*/
 const links = document.querySelectorAll('nav ul li a')
 
-for(const link of links){
-    link.addEventListener('click', () => {
-        nav.classList.remove('show')
-    })
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
 }
+
+/**mudar o header da pagina quando der scroll */
+const header = document.querySelector('header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
